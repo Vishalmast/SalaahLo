eda751e427eb0ceaed20af134b1f830fcf22fb1f
Vishal Kumar <kumarvishalmast@gmail.com>
2023-02-03 18:48:26 +0530

    first commit

---
+++ Client/src/app/header/header.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbAccordion } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ]
+})
+export class HeaderModule { }
