d455f5d16c68690083db8445249247ee283a2a72
Vishal Kumar <kumarvishalmast@gmail.com>
2023-02-03 22:19:13 +0530

    changes in UI

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbAccordion, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
